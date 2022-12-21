eb05758184d5971210f2197a4ee7f4961522eb84
kiranbaby14 <kiranbaby256@gmail.com>
2022-12-21 10:22:46 +0000

    complete frontend

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">HotelBooking</span>
+        <span className="logo">BookUrTrip</span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
